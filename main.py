aaa0d647185dc126feb238def9b96ca622c86f11
Karol Kozlowski <kozlowskikarol02@gmail.com>
2025-03-25 23:12:53 +0100

    [dev]: updating main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from antlr4 import *
 from lexer_parser.MyLangLexer import MyLangLexer
 from lexer_parser.MyLangParser import MyLangParser
+from AST.AST import ASTBuilder
 
 def main():
     input_stream = InputStream("int x; x = 5 + 3; print x;")
@@ -8,7 +9,16 @@ def main():
     tokens= CommonTokenStream(lexer)
     parser = MyLangParser(tokens)
     tree = parser.program()
+
+    print("Parse Tree:")
     print(tree.toStringTree(recog=parser))
+    
+    ast_builder = ASTBuilder()
+    ast = ast_builder.visit(tree)
+
+    print("AST Tree:")
+    for node in ast:
+        print(node)
 
 if __name__ == '__main__':
     main()
