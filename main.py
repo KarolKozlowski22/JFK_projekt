4f2a611e7e541d69be8c34b25e8cf1e0a7494b43
Karol Kozlowski <kozlowskikarol02@gmail.com>
2025-03-27 15:11:12 +0100

    [dev]: modyfing main.py examples and required python packages

--- main.py
+++ main.py
@@ -2,23 +2,30 @@ from antlr4 import *
 from lexer_parser.MyLangLexer import MyLangLexer
 from lexer_parser.MyLangParser import MyLangParser
 from AST.AST import ASTBuilder
+from helpers.helpers import read_examples, save_ir_module, save_ast, compile_ir
 
 def main():
-    input_stream = InputStream("int x; x = 5 + 3; print x;")
-    lexer = MyLangLexer(input_stream)
-    tokens= CommonTokenStream(lexer)
-    parser = MyLangParser(tokens)
-    tree = parser.program()
+    examples=read_examples()
+    it=1
+    for example in examples:
+        input_stream = InputStream(example[0])
+        lexer = MyLangLexer(input_stream)
+        tokens= CommonTokenStream(lexer)
+        parser = MyLangParser(tokens)
+        tree = parser.program()
 
-    print("Parse Tree:")
-    print(tree.toStringTree(recog=parser))
-    
-    ast_builder = ASTBuilder()
-    ast = ast_builder.visit(tree)
+        # print("Parse Tree:")
+        # print(tree.toStringTree(recog=parser))
+        
+        ast_builder = ASTBuilder()
+        ast, ir_module = ast_builder.visit(tree)
 
-    print("AST Tree:")
-    for node in ast:
-        print(node)
+        save_ast(ast, it)
+        save_ir_module(ir_module, it)
+        compile_ir(it)
+        it+=1
+
+
 
 if __name__ == '__main__':
     main()
